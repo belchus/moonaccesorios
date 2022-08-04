5b372037a96a95bf6339c38aa0f3df8aca6d2a0a
Camila Arena <arenabelu@gmail.com>
2022-08-04 16:11:53 -0300

    agrega events

--- js/index.js
+++ js/index.js
@@ -73,3 +73,46 @@ function sumar(produ1,produ2){
     return total
 }
 console.log(sumar(400,390))
+
+
+// getElementsByClassName('className')
+const boton = document.getElementsByClassName('boton')
+console.log(boton)
+
+boton.innerText = 'Agregar al Carrito'
+boton.style.color = "blue"
+
+const desc = document.getElementsByClassName('descripcion')
+desc.innerText = 'Agregar al Carrito'
+
+let formulario = document.querySelector('#formulario')
+let mensajeError = document.querySelector('.errorFormulario')
+let contrasena = document.querySelector('input[type="password"]')
+
+formulario.addEventListener('submit', validar)
+
+function validar(e){
+    e.preventDefault()
+    console.log(e)
+    let formArray = e.target
+    console.log(e)
+
+    let usuario = formArray[0]
+    let contrasena = formArray[1]
+
+
+
+    if(usuario.value.length < 5){
+        mensajeError.style.display = 'block'
+        mensajeError.innerText = '* Error! El usuario debe tener mas de 5 caracteres!!'
+        mensajeError.style.color = 'red'
+    }else{
+        mensajeError.style.display = 'none'
+    }
+    if(!contrasena.value){
+        mensajeError.style.display = 'block'
+        mensajeError.innerText = '* Error! la contraseña no puede estar vacia'
+        mensajeError.style.color = 'red'
+    }
+    
+}
