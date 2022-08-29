a9e606872faa48194ad6a1dd96f3615d3951cd1a
Camila Arena <arenabelu@gmail.com>
2022-08-29 19:27:38 -0300

    se agrego fetch

--- js/index.js
+++ js/index.js
@@ -113,7 +113,7 @@ usuario.value.length < 5?  mensajeError.innerText = 'El usuario ingresado es inc
 
 let container = document.querySelector('.mother')
 let article = ""
-
+/*
 for(producto of productos){
     console.log(producto)
   article += `<article class="son" >
@@ -128,6 +128,33 @@ for(producto of productos){
 }
 
 container.innerHTML += article
+*/
+fetch("https://my-json-server.typicode.com/belchus/parafetch/db")
+.then((response) => {
+    return response.json()
+})
+.then((pulseras) =>{
+    let mispulseras =pulseras.pulseras
+    console.log(mispulseras)
+    mispulseras.forEach(puls => {
+        const {imgSrc, id, titulo,precio,descripcion} = puls
+        article += `<article class="son" >
+                    <img src="${imgSrc}" alt="" class="sons">
+                    <div class="nomb">
+                        <p>${titulo}</p><div class="nomb">
+                        <p>${precio}</p>
+                    </div>
+                    </div>
+                    <div class="boton">
+                    <svg id="${id}" class="unlike" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M0 190.9V185.1C0 115.2 50.52 55.58 119.4 44.1C164.1 36.51 211.4 51.37 244 84.02L256 96L267.1 84.02C300.6 51.37 347 36.51 392.6 44.1C461.5 55.58 512 115.2 512 185.1V190.9C512 232.4 494.8 272.1 464.4 300.4L283.7 469.1C276.2 476.1 266.3 480 256 480C245.7 480 235.8 476.1 228.3 469.1L47.59 300.4C17.23 272.1 .0003 232.4 .0003 190.9L0 190.9z"/></svg>
+                    </div>
+                </article>`
+                container.innerHTML += article
+    
+})
+})
+
+
 
 let botones = document.querySelectorAll('.boton')
 console.log(botones)
@@ -236,5 +263,3 @@ const masProductos = {
 }
 
 console.log(masProductos)
-
-
