e34bc088a48bf0c4f6592e9aff63f3b79f316121
Camila Arena <arenabelu@gmail.com>
2022-09-14 23:14:12 -0300

    correcciones en secciones

--- js/carrito.js
+++ js/carrito.js
@@ -4,43 +4,24 @@ let storage = JSON.parse(recuperoStorage)
 console.log(storage)
 
 let carrito = [];
-if (recuperoStorage != null){
-carrito = JSON.parse(recuperoStorage)
-}else{
-    MostrarCarrito()
+if (recuperoStorage){
+    carrito=storage
+    MostrarCarrito(carrito)
+}else {
+    micarrito.innerHTML = "El carrito esta vacio"
 }
-function MostrarCarrito(){
-for (storage of storages){
-    storage.onclick = (e) => {
-        let element = e.target.parentElement
-        let id = element.attributes.id.value
-        let shop = pulsera.find(e => e.id == id)
-
-        if(carrito.includes(shop)){
-   element.classList.remove('liked')
-   let eliminoprod = carrito.indexOf(shop)
-   carrito.splice(eliminoprod,1)
-   let sacaRepetido = document.querySelector(`#id-${shop.id}`)
-   resultadosCarrito.removeChild(sacaRepetido)
-   swal("Eliminado del carrito", "", "error");
-        } else{
-            element.classList.add('liked')
-            carrito.push(shop)
-            swal("Agregado al carrito", "", "success");
-            micarrito.innerHTML += `<article class="son" id="id-${shop.id}" >
-            <img src="${shop.img}" alt="" class="sons">
-            <div class="nomb">
-                <p>${shop.nombre}</p>
-                <p>${shop.precio}</p>
-            </div>
-        </article>`
-        }
-
-        
-       
-localStorage.setItem('carrito',JSON.stringify(carrito))
-
-
-}
-}
+function MostrarCarrito(storage){
+    for (product of storage){
+        const {imgSrc, id, titulo} = product
+        micarrito.innerHTML = `<article class="son" >
+        <img src="${imgSrc}" alt="" class="sons">
+        <div class="nomb">
+            <p>${titulo}</p><div class="nomb">
+        </div>
+        <div class="boton  boton-${id}">Ver detalle</div>
+                        <input type='hidden' class='info-id' value="${id}"/>
+                    </div>
+        </div>
+    </article>`
+    }
 }
